00b1e4f17b00291f8567f4c99beaa2bdb34e8f3c
LeonardoCera <leo.supponente@gmail.com>
2026-02-18 14:23:30 +0100

    Abstracts

--- src/routes/writings/+page.svelte
+++ src/routes/writings/+page.svelte
@@ -6,19 +6,31 @@
 </script>
 
 <div class="container">
-    <h2>Published Articles:</h2>
+    <h2>Journal Articles:</h2>
     <ul>
         <li style="font-size: 18px"> Leonardo Ceragioli, <a href={Peano}> Peano's counterexample to Harmony </a> Theoria 85 (6), pp. 459-484. DOI: <a href="https://doi.org/10.1111/theo.12217"> https://doi.org/10.1111/theo.12217 </a> (2019) </li>
-        <li style="font-size: 18px"> Leonardo Ceragioli, <a href={Assurdo}> Ex falso and Proof-Theoretic Validity </a> in Bellotti e Turbanti (ed.) Fourth Pisa Colloquium in Logic, Language and Epistemology, Edizioni ETS (2021). </li>
+        
         <li style="font-size: 18px"> Leonardo Ceragioli, <a href={Tennant}> New problems for Tennant's definition of harmony </a> Theoria 88 (4), pp. 829-849. DOI: <a href="https://doi.org/10.1111/theo.12400"> https://doi.org/10.1111/theo.12400 </a> (2022) </li>
         <li style="font-size: 18px"> Leonardo Ceragioli, Single-Assumption Systems in Proof-Theoretic Semantics Journal of Philosophical Logic 51, pp. 1019-1054. DOI: <a href="https://doi.org/10.1007/s10992-022-09658-4"> https://doi.org/10.1007/s10992-022-09658-4 </a> (2022) </li>
         <li style="font-size: 18px"> Leonardo Ceragioli and Valentina Luporini, Intuition is not (always) immediate, and this is good news! Bulletin d'Analyse Phénoménologique XIX Numéro 3 (Actes 13), pp. 180–204. <a href="https://popups.uliege.be/1782-2041/index.php?id=1402&quot;"> https://popups.uliege.be/1782-2041/index.php?id=1402 </a> (2023) </li>
-        <li style="font-size: 18px"> Leonardo Ceragioli, <a href={Ecumenical}> Which Ecumenical System for Classical and Intuitionistic Logics? </a> in Bellotti, Moriconi e Turbanti (ed.) Fifth Pisa Colloquium in Logic, Language and Epistemology, Edizioni ETS (2023). </li>
         <li style="font-size: 18px"> Leonardo Ceragioli, Bilateral Rules as Complex Rules Bulletin of the Section of Logic 52(3), pp. 329-375. DOI: <a href=" https://doi.org/10.18778/0138-0680.2023.13"> https://doi.org/10.18778/0138-0680.2023.13 </a> (2023) </li>
         <li style="font-size: 18px"> Leonardo Ceragioli, Inferential Semantic Contamination, Harmony and Realist Pollution Synthese 205(123). DOI: <a href=" https://doi.org/10.1007/s11229-024-04871-x"> https://doi.org/10.1007/s11229-024-04871-x </a> (2025) </li>
         <li style="font-size: 18px"> Leonardo Ceragioli and Giuseppe Primiero, A Proof System with Causal Labels (Part II): checking Counterfactual Fairness, OVERLAY 2025 proceedings: <a href=" https://ceur-ws.org/Vol-4142/paper19.pdf "> https://ceur-ws.org/Vol-4142/paper19.pdf </a> (2025) </li>
         <li style="font-size: 18px"> Leonardo Ceragioli and Giuseppe Primiero, A Proof System with Causal Labels (Part I): checking Individual Fairness and Intersectionality, OVERLAY 2025 proceedings: <a href=" https://ceur-ws.org/Vol-4142/paper19.pdf "> https://ceur-ws.org/Vol-4142/paper19.pdf </a> (2025) </li>
         <li style="font-size: 18px"> Leonardo Ceragioli and Giuseppe Primiero, Trustworthiness Preservation by Copies of Machine Learning Systems, ArXiv: <a href=" https://doi.org/10.48550/arXiv.2506.05203 "> https://doi.org/10.48550/arXiv.2506.05203 </a> (2025) </li>
+    </ul>
+    <h2>Contributions in Books:</h2>
+    <ul>
+        <li style="font-size: 18px"> Leonardo Ceragioli, <a href={Ecumenical}> Which Ecumenical System for Classical and Intuitionistic Logics? </a> in Bellotti, Moriconi e Turbanti (ed.) Fifth Pisa Colloquium in Logic, Language and Epistemology, Edizioni ETS (2023). </li>
+        <li style="font-size: 18px"> Leonardo Ceragioli, <a href={Assurdo}> Ex falso and Proof-Theoretic Validity </a> in Bellotti e Turbanti (ed.) Fourth Pisa Colloquium in Logic, Language and Epistemology, Edizioni ETS (2021). </li>
+    </ul>
+    <h2>Published Abstracts:</h2>
+    <ul>
+
+    <li style="font-size: 18px"> Leonardo Ceragioli, Proof-Theoretic Semantics and Anti-Exceptionalism, abstract in the Book
+of abstract of the Workshop Realism and anti-realism. Paradigms and research programmes in logic and the philosophy of mathematics, held at the Carl Friedrich von Weizsäcker Center of the University of Tübingen on April 28th-29th 2025, pp. 32–35.: <a href=" http://dx.doi.org/10.15496/publikation-108520 "> http://dx.doi.org/10.15496/publikation-108520 </a> (2025) </li>
+
+    </ul>
     <h2>Preprints Articles:</h2>
     <ul>
     <li style="font-size: 18px"> Leonardo Ceragioli and Giuseppe Primiero, A Proof System with Causal Labels (Part II): checking Counterfactual Fairness, Arxiv: <a href=" https://doi.org/10.48550/arXiv.2507.14655 "> https://doi.org/10.48550/arXiv.2507.14655 </a> (2025) </li>
